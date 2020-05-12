db384c5e939f1108cbb838914e8623f78b6c9323
jfzibu <59874432+jfzibu@users.noreply.github.com>
2020-05-12 16:39:51 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+ //Stickup J query plugin
+ jQuery(function($) {
+             $(document).ready(function() {
+                 $('.navbar-wrapper').stickUp({
+
+                 });
+             });
